b2c778a212b1aa5eacd413f2d466cfa8d4e2caf6
jeremiah ojo <ojoj665@gmail.com>
2023-02-24 14:34:28 +0100

    custom interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomInterceptor } from './custom.interceptor';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
     AppService,
-    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: CustomInterceptor },
   ],
 })
 export class AppModule {}
